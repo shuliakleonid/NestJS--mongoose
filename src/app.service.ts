4cb0b22dc4419a2908b11d5d033be93a59af0d8f
lshuliak <zeleny777@gmail.com>
2022-09-05 13:37:38 +0300

    test husky

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
+
+
+
+  
+
   getHello(): string {
-    return "Hello World!";
+
+
+
+    return "Hello World!   ";
+
   }
 }
