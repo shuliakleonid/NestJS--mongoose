46ffee8427d74b9c6488477ba3d3101b3ef24b6b
lshuliak <zeleny777@gmail.com>
2022-09-05 13:26:30 +0300

    init nest application

---
+++ .eslintrc.js
@@ -0,0 +1,119 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		"createDefaultProgram": true,
+    "project": "tsconfig.json",
+    "sourceType": "module",
+    "tsconfigRootDir": __dirname,
+	},
+	ignorePatterns: ['/*.*'],
+	plugins: [
+		'@typescript-eslint/eslint-plugin',
+		'prettier',
+		'unicorn',
+		'import',
+		'no-secrets',
+	],
+	extends: [
+		'plugin:import/typescript',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:import/recommended',
+		'plugin:import/errors',
+		'plugin:unicorn/all',
+		'prettier',
+	],
+	root: true,
+	settings: {
+    "import/resolver": {
+      "typescript": {
+				"alwaysTryTypes": true,
+				"project": './tsconfig.json'
+
+			}
+    },
+  },
+	env: {
+		node: true,
+		jest: true,
+		es6: true,
+		browser: false,
+	},
+	rules: {
+		'no-duplicate-imports': 'error',
+		'no-param-reassign': 'error',
+		'unicorn/filename-case': 'off',
+		'no-secrets/no-secrets': 'error',
+		'no-array-constructor': 'error',
+		'no-mixed-operators': 'error',
+		'import/default': 'off',
+		'no-plusplus': [
+			"error",
+			{ "allowForLoopAfterthoughts": true }
+		],
+		'import/prefer-default': 'off',
+		'unicorn/prevent-abbreviations': [
+			"error",
+	{
+		"ignore": [
+			"\\.e2e*",
+			"\\.spec*",
+			"\\*idx*",
+		]
+	}
+		],
+		'import/namespace': 'off',
+		"unicorn/prefer-top-level-await": "off",
+		'unicorn/no-null': 'off',
+		'unicorn/import-style': 'off',
+		'unicorn/prefer-module': 'off',
+		'unicorn/prefer-node-protocol': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{ argsIgnorePattern: '^_' },
+		],
+		"no-secrets/no-secrets":["error",{"ignoreContent":"^(operations.|exception.|validation.)"}],
+		'no-multi-spaces': 'error',
+		'linebreak-style': ['error', 'unix'],
+		'newline-before-return': 'error',
+		'no-await-in-loop': 'error',
+		'padding-line-between-statements': [
+			'error',
+			{
+				blankLine: 'always',
+				prev: ['const', 'let', 'var'],
+				next: '*',
+			},
+			{
+				blankLine: 'any',
+				prev: ['const', 'let', 'var'],
+				next: ['const', 'let', 'var'],
+			},
+		],
+		'prefer-const': [
+			'error',
+			{
+				destructuring: 'any',
+				ignoreReadBeforeAssign: false,
+			},
+		],
+		quotes: [
+			'error',
+			'double',
+			{
+				allowTemplateLiterals: true,
+				avoidEscape: true,
+			},
+		],
+		"no-unused-vars": ["error", { "varsIgnorePattern": "^_","ignoreRestSiblings": true,"argsIgnorePattern": "^_" }],
+		'no-console': [
+			'warn',
+			{
+				allow: ['warn', 'error', 'info','table'],
+			},
+		],
+	},
+};
